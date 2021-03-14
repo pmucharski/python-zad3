4a8f88cb1a58dfe2c8b45a751a69bb1cbe228882
pmucharski <pmucharski2k@gmail.com>
2021-03-14 19:36:26 +0100

    wizualizacja danych

--- main.py
+++ main.py
@@ -24,3 +24,68 @@
 # print(sklep)
 # odwrocone = {value: key for key, value in sklep.items()}
 # print(odwrocone)
+
+#zad 4
+# def check(a, b, c):
+#     if a == b == c:
+#         return 'Trójkąt równoboczny'
+#     if a >= b and a >= c:
+#         if a ** 2 == b ** 2 + c ** 2:
+#             return 'Trójkąt prostokątny'
+#         else:
+#             return 'Trójkąt nie jest prostokątny'
+#     elif b >= a and b >= c:
+#         if b ** 2 == a ** 2 + c ** 2:
+#             return 'Trójkąt prostokątny'
+#         else:
+#             return 'Trójkąt nie jest prostokątny'
+#     else:
+#         if c ** 2 == b ** 2 + a ** 2:
+#             return 'Trójkąt prostokątny'
+#         else:
+#             return 'Trójkąt nie jest prostokątny'
+#
+#
+# print(check(3, 4, 5))
+#
+# #zad 5
+# def trapez(a=3, b=4, h=6):
+#     return ((a + b) * h) / 2
+#
+#
+# print(trapez())
+#
+# #zad 6
+# # Parametry funkcji a1 (wartość początkowa), b (wielkość o ile mnożone są kolejne elementy), ile (ile elementów ma mnożyć)
+# # Ponadto funkcja niech przyjmuje wartości domyślne: a = 1, b = 4, ile = 10
+# def iloczyn(a=1, b=4, ile=10):
+#     for elem in range(a, ile):
+#         print(elem * b)
+#
+#
+# iloczyn(3, 3, 13)
+#
+# #zad 7
+# def fun(*liczby):
+#     if len(liczby) != 3:
+#         print('Podaj trzy liczby!')
+#         return -1
+#     for elem in range(liczby[0], liczby[2]):
+#         print(elem * liczby[1])
+#
+#
+# fun(1, 5, 4)
+#
+# #zad 8
+# def licz(**kwargs):
+#     return len(kwargs.items()), sum(x for x in kwargs.values())
+#
+#
+# print(licz(mleko=15, zupa=3, kalafior=7))
+#
+# #zad 9
+# from ciagi import *
+#
+# print(arytmetyczne.nwyraza(1, 10, 2))
+# print(geometryczne.sumanwyrazow(1, 5, 5))
+
